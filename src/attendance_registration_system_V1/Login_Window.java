90d2df145b1b0b5ddd07bf0042ef5b195fc0588d
Ibtihaj <ibtihaj_naeem@yahoo.it>
2020-01-07 23:41:39 +0000

    Created Date Calculator class
    Created a class to calculate date, static time and Hijri date. Also created a status bar that displays this information. Created a separate class so that the method can be called on each individual JFrame.

--- src/attendance_registration_system_V1/Login_Window.java
+++ src/attendance_registration_system_V1/Login_Window.java
@@ -1,31 +1,27 @@
 package attendance_registration_system_V1;
 
 import java.awt.EventQueue;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.BevelBorder;
-
-import java.awt.Color;
-import javax.swing.JSeparator;
-import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
-import javax.swing.ImageIcon;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 
 public class Login_Window {
 
+	private DateCalculator dateCalc;
+	
 	private JFrame frmAttendanceRegistrationSystem;
-	private JPanel statusBarTime;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
-
+    private JMenuBar statusBar;
+    private JMenu dateText;
+    private JMenu timeText;
+    private JMenu islamicDateText;
+    
 	/**
 	 * Launch the application.
 	 */
@@ -47,92 +43,85 @@ public class Login_Window {
 	 */
 	public Login_Window() {
 		initialize();
+		dateCalc.currentDate(dateText, timeText, islamicDateText);
 	}
 
 	/**
-	 * Obtains time and date from system to display on status bar
-	 */
-	public void CurrentDate(){ // this method is used to calculate the gregorian date. The reason I am adding to the month
-	    Calendar cal = new GregorianCalendar(); // is because the system counts January as month zero therefore to get the actual month I have to add 1
-	    int day = cal.get(Calendar.DAY_OF_MONTH);
-	    int month = cal.get(Calendar.MONTH);
-	    int year = cal.get(Calendar.YEAR);
-	    //menuDate.setText(day + "/" + (month+1) + "/"  +year); // This prints the date as intended
-	    
-	    int seconds = cal.get(Calendar.SECOND); // this method is used to calculate the time in the specific moment the program is opened
-	    int minute = cal.get(Calendar.MINUTE);
-	    int hour = cal.get(Calendar.HOUR);  
-	    //menuTime.setText(hour + ":" + minute + ":" + seconds); // This prints the time in an HH:MM:SS format 
-	    }
-
-	/**
-	 * Initialize the contents of the frame.
+	 * Initialise the contents of the frame.
 	 */
 	public void initialize() {
+		
+		dateCalc = new DateCalculator();
+		
 		frmAttendanceRegistrationSystem = new JFrame();
 		frmAttendanceRegistrationSystem.setTitle("Attendance Registration System");
-		frmAttendanceRegistrationSystem.setBounds(100, 100, 450, 300);
+		frmAttendanceRegistrationSystem.setBounds(100, 100, 460, 320);
 		frmAttendanceRegistrationSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAttendanceRegistrationSystem.getContentPane().setLayout(null);
 		
-		JLabel menuDate = new JLabel("Date");
-		menuDate.setBounds(12, 0, 68, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(menuDate);
-		
-		JPanel statusBarDate = new JPanel();
-		statusBarDate.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.BLACK, null, null, null));
-		statusBarDate.setBounds(0, 0, 80, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(statusBarDate);
-		
-		JSeparator separator = new JSeparator();
-		separator.setOrientation(SwingConstants.VERTICAL);
-		separator.setBounds(80, 0, 2, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(separator);
-		
-		JLabel menuTime = new JLabel("Time");
-		menuTime.setHorizontalAlignment(SwingConstants.LEFT);
-		menuTime.setBounds(92, 0, 68, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(menuTime);
-		
-		statusBarTime = new JPanel();
-		statusBarTime.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.BLACK, null, null, null));
-		statusBarTime.setBounds(80, 0, 80, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(statusBarTime);
-		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setOrientation(SwingConstants.VERTICAL);
-		separator_1.setBounds(160, 0, 2, 20);
-		frmAttendanceRegistrationSystem.getContentPane().add(separator_1);
-		
 		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
-			}
-		});
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                exitButtonActionPerformed(e);
+            }
+        });
 		exitButton.setBounds(12, 215, 97, 25);
 		frmAttendanceRegistrationSystem.getContentPane().add(exitButton);
 		
 		JButton loginButton = new JButton("Login");
-		loginButton.setBounds(323, 215, 97, 25);
+        loginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+            	loginButtonActionPerfomed(e);
+            }
+        });
+		loginButton.setBounds(333, 215, 97, 25);
 		frmAttendanceRegistrationSystem.getContentPane().add(loginButton);
 		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(115, 83, 68, 16);
+		lblUsername.setBounds(120, 74, 68, 16);
 		frmAttendanceRegistrationSystem.getContentPane().add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(115, 133, 56, 16);
+		lblPassword.setBounds(120, 124, 56, 16);
 		frmAttendanceRegistrationSystem.getContentPane().add(lblPassword);
 		
 		usernameField = new JTextField();
-		usernameField.setBounds(195, 80, 116, 22);
+		usernameField.setBounds(200, 71, 116, 22);
 		frmAttendanceRegistrationSystem.getContentPane().add(usernameField);
 		usernameField.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(195, 130, 116, 22);
+		passwordField.setBounds(200, 121, 116, 22);
 		frmAttendanceRegistrationSystem.getContentPane().add(passwordField);
+		
+		statusBar = new JMenuBar();
+		frmAttendanceRegistrationSystem.setJMenuBar(statusBar);
+		
+        dateText = new JMenu();
+        dateText.setText("Date");
+		statusBar.add(dateText);
+		
+		islamicDateText = new JMenu("Islamic Date");
+		statusBar.add(islamicDateText);
+		
+		timeText = new JMenu("Time");
+		statusBar.add(timeText);
 
 	}
+	
+	/**
+	 * Method to execute when the event from the exit button occurs
+	 */
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent e) {
+        System.exit(0);
+    }
+    
+	/**
+	 * Method to execute when the event from the login button occurs
+	 */
+    private void loginButtonActionPerfomed(java.awt.event.ActionEvent e) {
+    	frmAttendanceRegistrationSystem.dispose();
+        Main_Window.main(null);
+    }
+    
 }
--- src/attendance_registration_system_V1/Main_Window.java
+++ src/attendance_registration_system_V1/Main_Window.java
@@ -3,10 +3,18 @@ package attendance_registration_system_V1;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 
 public class Main_Window {
 
+	private DateCalculator dateCalc;
+	
 	private JFrame frame;
+    private JMenuBar statusBar;
+    private JMenu dateText;
+    private JMenu timeText;
+    private JMenu islamicDateText;
 
 	/**
 	 * Launch the application.
@@ -29,15 +37,32 @@ public class Main_Window {
 	 */
 	public Main_Window() {
 		initialize();
+		dateCalc.currentDate(dateText, timeText, islamicDateText);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		dateCalc = new DateCalculator();
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		statusBar = new JMenuBar();
+		frame.setJMenuBar(statusBar);
+		
+        dateText = new JMenu();
+        dateText.setText("Date");
+		statusBar.add(dateText);
+		
+		islamicDateText = new JMenu("Islamic Date");
+		statusBar.add(islamicDateText);
+		
+		timeText = new JMenu("Time");
+		statusBar.add(timeText);
 	}
 
 }
